e3ea2bb95d3c2ea050ebc20b4ef2bca7f716e591
Zhanfeng <993134187@qq.com>
2016-07-17 23:24:16 +0800

    add file

---
+++ test3.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>练习3</title>
+    <link href="http://apps.bdimg.com/libs/bootstrap/3.3.4/css/bootstrap.css" rel="stylesheet"> 
+    <script src="http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="http://apps.bdimg.com/libs/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+</head>
+<body style="padding-top:20px;padding-bottom:20px;">
+    <div class="container">
+        <div class="header clearfix">
+            <nav>
+                <ul class="nav nav-pills pull-right">
+                    <li role="presentation" class="active">
+                        <a href="#">Home</a>
+                    </li>
+                    <li role="presentation">
+                        <a href="#">About</a>
+                    </li>
+                    <li class="presentation">
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+            <h3 class="text-muted">KCN</h3>
+        </div>
+        <div class="jumbotron">
+            <h1>暑假练习</h1>
+            <p class="lead">
+                暑假特训，明天上班去，下午下班回家撸代码！
+            </p>
+            <p>
+                <a class="btn btn-lg btn-success" href="#" role="button">Sign up today</a>
+            </p>
+        </div>
+        <div class="row marketing">
+            <div class="col-lg-6">
+                <h4>副标题</h4>
+                <p>这里是内容</p>
+            </div>
+        </div>
+        <footer class="footer">
+            <p>© Company 2016</p>
+        </footer>
+    </div>
+</body>
+</html>
---
+++ test5.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>练习5</title>
+    <link href="http://apps.bdimg.com/libs/bootstrap/3.3.4/css/bootstrap.css" rel="stylesheet"> 
+    <script src="http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="http://apps.bdimg.com/libs/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="site-wrapper">
+        <div class="site-wrapper-inner">
+            <div class="cover-container">
+                <div class="masthead clearfix">
+                    <div class="inner">
+                      <h3 class="masthead-brand">Cover</h3>
+                      <nav>
+                        <ul class="nav masthead-nav">
+                          <li class="active"><a href="#">Home</a></li>
+                          <li><a href="#">Features</a></li>
+                          <li><a href="#">Contact</a></li>
+                        </ul>
+                      </nav>
+                    </div>
+                </div>
+                <div class="inner cover">
+                    <h1 class="cover-heading">Cover your page.</h1>
+                    <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+                    <p class="lead">
+                      <a href="#" class="btn btn-lg btn-default">Learn more</a>
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</body>
+</html>
