82fb533ecfb56f9cd9d4e02388361a8d4a18f6f7
Zhanfeng <993134187@qq.com>
2016-07-19 16:21:58 +0800

    add file

---
+++ test7.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--上面三个mate标签必须最前面-->
+    <title>练习7</title>
+    <link rel="stylesheet" type="text/css" href="test7.css">
+    <link href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+    <link href="http://apps.bdimg.com/libs/bootstrap/3.3.4/css/bootstrap.css" rel="stylesheet"> 
+    <script src="http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="http://apps.bdimg.com/libs/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="blog-masthead">
+        <div class="container">
+            <nav class="blog-nav">
+                <a href="#" class="blog-nav-item active">
+                    Home
+                </a>
+                <a href="#" class="blog-nav-item">
+                    New features
+                </a>
+                <a href="#" class="blog-nav-item">
+                    Press
+                </a>
+                <a href="#" class="blog-nav-item">
+                    New hires
+                </a>
+                <a href="#" class="blog-nav-item">
+                    About
+                </a>
+            </nav>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="blog-header">
+            <h1 class="blog-title">The Bootstrap Blog</h1>
+            <p class="lead blog-description">The official example template of creating a blog with Bootstrap.</p>
+        </div>
+        <div class="row">
+            <div class="col-sm-8 blog-main">
+                <div class="blog-post">
+                    <h2 class="blog-post-title">Sample blog post</h2>
+                    <p class="blog-post-meta">January 7, 2016 by <a href="#">Mark</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
